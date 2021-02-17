69dc9c3b341cf544aaacda2e08a83c406d688b04
Ahmed Nady <ahmednady@fci.helwan.edu.eg>
2021-02-17 22:51:02 +0200

    Update coco.py

--- datasets/coco.py
+++ datasets/coco.py
@@ -159,8 +159,8 @@ def build(image_set, args):
     assert root.exists(), f'provided COCO path {root} does not exist'
     mode = 'instances'
     PATHS = {
-        "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
-        "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
+        "train": (root / "train", root / "annotations" / f'{mode}_train.json'),
+        "val": (root / "val", root / "annotations" / f'{mode}_val.json'),
     }
 
     img_folder, ann_file = PATHS[image_set]
