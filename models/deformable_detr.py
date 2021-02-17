1212c970d85a11132bd77a4785e903c961037d01
Ahmed Nady <ahmednady@fci.helwan.edu.eg>
2021-02-17 22:53:10 +0200

    Update deformable_detr.py

--- models/deformable_detr.py
+++ models/deformable_detr.py
@@ -442,7 +442,7 @@ class MLP(nn.Module):
 
 
 def build(args):
-    num_classes = 20 if args.dataset_file != 'coco' else 91
+    num_classes = 2 if args.dataset_file != 'coco' else 91
     if args.dataset_file == "coco_panoptic":
         num_classes = 250
     device = torch.device(args.device)
