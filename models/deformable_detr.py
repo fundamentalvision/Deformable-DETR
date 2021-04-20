1ba354e2e24869216a70679d5df9414040879a9f
Swall0w <technext.jpn@gmail.com>
2021-04-20 22:55:04 +0900

    [update]: bug fix.

--- models/deformable_detr.py
+++ models/deformable_detr.py
@@ -126,7 +126,9 @@ class DeformableDETR(nn.Module):
                - "aux_outputs": Optional, only returned when auxilary losses are activated. It is a list of
                                 dictionnaries containing the two above keys for each decoder layer.
         """
-        if not isinstance(samples, NestedTensor):
+#        if not isinstance(samples, NestedTensor):
+#            samples = nested_tensor_from_tensor_list(samples)
+        if isinstance(samples, (list, torch.Tensor)):
             samples = nested_tensor_from_tensor_list(samples)
         features, pos = self.backbone(samples)
 
